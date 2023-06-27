fe6b873cb84029b7a5e46633d63f4c40be78e06a
unknown <maxi.lizarraga.91@gmail.com>
2023-06-27 12:02:01 -0300

    se agrega express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hola node")
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req,res) => {
+    res.send("Hola")
+} )
+
+app.listen(3000, () =>{
+    console.log("Servidor funcionando")
+})
